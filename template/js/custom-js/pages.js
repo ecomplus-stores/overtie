9da834c3d7d8d36b3aec47330f192c8e8bf3f9df
Eduardo <edu.vlemes@gmail.com>
2024-09-20 00:16:20 -0300

    .

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,6 @@
 // Add your custom JavaScript for storefront pages here.
+document.addEventListener("DOMContentLoaded", function() {
+  $(`#mobile-search-btn`).click(function(){
+    $(`.header__search-input`).focus()
+  })
+});
